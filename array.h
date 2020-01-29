ba578ab118d4d01182a2653415caf94272fc87ec
Isaac Lin <q331074685@gmail.com>
2020-01-29 18:23:06 -0500

    more classes

--- array.h
+++ array.h
@@ -1,45 +1,184 @@
 #pragma once
 
+#include <cstdlib>
 #include "object.h"
 #include "string.h"
 
 // assuming an array is a java-style arraylist
-class Array : public Object
-{
-    public:
-        //constructor
-       Array();
+class Array : public Object {
+   public:
 
-        //destructor
-       ~Array();
+    // constructor
+    Array() {}
 
-        // hash and return the hash value
-        virtual size_t hash_me();
+    // destructor
+    ~Array() {}
 
-        // putting an item at the end of the array
-        virtual void append(Object* oo);
+    // putting an item at the end of the array
+    virtual void append(Object* oo) {}
 
-        // get the n'th item in the array.
-        virtual Object* get(int nn);
+    // get the n'th item in the array
+    Object* get(int nn) {}
 
-        // returns the length of the array.
-        virtual int length();
+    // remove the n'th item in the array
+    // returning the removed item to the caller
+    Object* remove(int nn) {}
 
-        // set n'th element to the given object
-        // returns the replaced Object.
-        virtual Object* set(int nn, Object *oo);
+    // returns the length of the array.
+    virtual int length() {}
 
-        // get the index of an object in the array
-        virtual int index_of(Object* oo);
+    // set n'th element to the given object
+    // returns the replace Object.
+    virtual Object* set(int nn, Object* oo) {}
 
-        // this compares two arrays, to see if they are the same
-        virtual bool equals(Object *oo);
+    // get the index of an object in the array
+    virtual int index_of(Object* oo) {}
 
-        // remove an element from the array
-        // returns the element removed
-        virtual Object* remove(int nn);
+    // Compares other with this array for equality.
+    bool equals(Object* oo) {}
 
-        // print content
-        // -1 on failure, 0 on success
-        virtual void print();
+    size_t hash() {}
+};
+
+
+
+class IntArray : public Array {
+ public:
+
+
+  // constructor
+  IntArray() {}
+
+  // destructor
+  ~IntArray() {}
+
+  void append(Object* oo) {}
+
+  // putting an item at the end of the array
+  void append(int oo) {}
+
+  // get the n'th item in the array
+  int get(int nn) {}
+
+  // remove the n'th item in the array
+  // returning the removed item to the caller
+  int remove(int nn) {}
+
+  Object* set(int nn, Object* oo) {}
+
+  // set n'th element to the given element
+  // returns the replace Object.
+  int set(int nn, int oo) {}
+
+  int index_of(Object* oo) {}
+
+  // get the index of an element in the array
+  int index_of(int oo) {}
+
+  // Compares other with this array for equality.
+  bool equals(Object* oo) {}
+
+  size_t hash() {}
+};
+
+class FloatArray : public Array {
+ public:
+
+  // constructor
+  FloatArray() {}
+
+  // destructor
+  ~FloatArray() {}
+
+  void append(Object* oo) {}
+
+  // putting an item at the end of the array
+  void append(float oo) {}
+
+  // get the n'th item in the array
+  float get(int nn) {}
+
+  // remove the n'th item in the array
+  // returning the removed item to the caller
+  float remove(int nn) {}
+
+  Object* set(int nn, Object* oo) {}
+
+  // set n'th element to the given element
+  // returns the replace Object.
+  float set(int nn, float oo) {}
+
+  int index_of(Object* oo) {}
+
+  // get the index of an float in the array
+  int index_of(float oo) {}
+
+  // Compares other with this array for equality.
+  bool equals(Object* oo) {}
+
+  size_t hash() {}
+};
+
+class BoolArray : public Array {
+ public:
+  // constructor
+  BoolArray() {}
+
+  // destructor
+  ~BoolArray() {}
+
+  void append(Object* oo);
+
+  // putting an item at the end of the array
+  void append(bool oo);
+
+  // get the n'th item in the array
+  bool get(int nn);
+
+  // remove the n'th item in the array
+  // returning the removed item to the caller
+  bool remove(int nn);
+
+  Object* set(int nn, Object* oo);
+
+  // set n'th element to the given element
+  // returns the replace Object.
+  bool set(int nn, bool oo);
+
+  int index_of(Object* oo);
+
+  // get the index of an element in the array
+  int index_of(bool oo);
+
+  // Compares other with this array for equality.
+  bool equals(Object* oo);
+
+  size_t hash();
+};
+
+class StringArray : public Array {
+ public:
+
+  // constructor
+  StringArray() {}
+
+  // destructor
+  ~StringArray() {}
+
+  // putting an item at the end of the array
+  void append(Object* oo);
+
+  void append(String* oo);
+
+  String* get(int nn);
+
+  // remove the n'th item in the array
+  // returning the removed item to the caller
+  String* remove(int nn);
+
+  Object* set(int nn, Object* oo);
+
+  // set n'th element to the given object
+  // returns the replace Object.
+  String* set(int nn, String* oo);
 };
