4c37054228c4784656bd5116cd143996e1d46c2c
Isaac Lin <q331074685@gmail.com>
2020-01-29 02:05:22 -0500

    update 7

--- object.h
+++ object.h
@@ -12,29 +12,28 @@
  * A class that represents the top of the object hierarchy.                      
  * author: vitekj@me.com */                                                      
 class Object : public Sys {                                                      
- public:                                                                         
-  size_t hash_;  // cache the hash value                                         
+ public:                                       
                                                                                  
   /** Default constructor ensure that hash is initialized */                     
-  Object() { hash_ = 0; }                                                        
+  Object();                                             
                                                                                  
   /** Default constructor is virtual to support subclasses */                    
-  virtual ~Object() {}                                                           
+  virtual ~Object();                                                           
                                                                                  
   /** Returns the object's hash value. Two objects that are equal should         
    * have the same hash; object's that are not, may have the same hash.          
    * Subclasses should implement hash_me_(). */                                  
-  size_t hash() {  return  hash_ ? hash_ :  hash_ = hash_me_();  }               
+  size_t hash();               
                                                                                  
   /** Trivial hash function. */                                                  
-  virtual size_t hash_me_() { return 0; }                                        
+  virtual size_t hash_me_();                                       
                                                                                  
   /** Use the address of the other object to determine equality. */              
-  virtual bool equals(Object* other) { return this == other; }                   
-                                                                                 
+  virtual bool equals(Object* other);
+                                               
   /** Return a newly allocated string describin the object */                    
-  virtual char* to_string() { return duplicate("Object"); }                      
-                                                                                 
+  virtual char* to_string();
+                                                        
   /** Print to stdout. */                                                        
-  virtual void print() { pln("object");  }                                                                                                                    
+  virtual void print();                                                                                                                 
 };   
