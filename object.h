63eeb004d77232a875c3a9262a7a8578da18ece0
Isaac Lin <q331074685@gmail.com>
2020-01-29 01:20:39 -0500

    standard object:

--- object.h
+++ object.h
@@ -1,34 +1,40 @@
-#pragma once
-#include <stdlib.h>
-#include <iostream>
+// --------------------------------README--------------------------------------------------
 
-class Object
-{
-    public:
-        size_t hash__;
+// the following is written by Jan Vltek
+// can be found on https://piazza.com/class/k51bluky59n2jr?cid=166
 
-        Object()
-        {
-            hash__ = 0;
-        }
+// ----------------------------------------------------------------------------------------
 
-        virtual ~Object()
-        {
-            
-        }
-
-        size_t hash()
-        {
-            return hash__ ? hash__ : hash__ = hash_me();
-        }
-
-        virtual size_t hash_me()
-        {
-            return 0;
-        }
-
-        virtual void print()
-        {
-            
-        }                                                                                                                                          
-};
+#pragma once                                                                     
+//lang::CwC                                                                                                                                                       
+#include "helper.h"                                                                                                                                               
+/**                                                                              
+ * A class that represents the top of the object hierarchy.                      
+ * author: vitekj@me.com */                                                      
+class Object : public Sys {                                                      
+ public:                                                                         
+  size_t hash_;  // cache the hash value                                         
+                                                                                 
+  /** Default constructor ensure that hash is initialized */                     
+  Object() { hash_ = 0; }                                                        
+                                                                                 
+  /** Default constructor is virtual to support subclasses */                    
+  virtual ~Object() {}                                                           
+                                                                                 
+  /** Returns the object's hash value. Two objects that are equal should         
+   * have the same hash; object's that are not, may have the same hash.          
+   * Subclasses should implement hash_me_(). */                                  
+  size_t hash() {  return  hash_ ? hash_ :  hash_ = hash_me_();  }               
+                                                                                 
+  /** Trivial hash function. */                                                  
+  virtual size_t hash_me_() { return 0; }                                        
+                                                                                 
+  /** Use the address of the other object to determine equality. */              
+  virtual bool equals(Object* other) { return this == other; }                   
+                                                                                 
+  /** Return a newly allocated string describin the object */                    
+  virtual char* to_string() { return duplicate("Object"); }                      
+                                                                                 
+  /** Print to stdout. */                                                        
+  virtual void print() { pln("object");  }                                                                                                                    
+};   
